b7bbd6e3cda322ed888871ca8d1017675ba9c84a
Jannick <52700380+j-siderius@users.noreply.github.com>
2026-03-26 13:49:07 +0100

    Update tinyml-board-tests.ino to relieve microphone test failing

--- assets/example-code/tinyml-board-tests/tinyml-board-tests.ino
+++ assets/example-code/tinyml-board-tests/tinyml-board-tests.ino
@@ -5,7 +5,7 @@
 #include <SparkFunBME280.h>
 #include "SparkFun_BMI270_Arduino_Library.h"
 #include <FastLED.h>
-#include <driver/i2s.h>
+// #include <driver/i2s.h>
 
 #define pin_button1 0
 #define pin_analog1 1
@@ -33,28 +33,28 @@ BMI270 imuSensor;
 #define NUM_LEDS 5
 CRGB leds[NUM_LEDS];
 
-#define SAMPLE_BUFFER_SIZE 512
-#define SAMPLE_RATE 8000
-#define I2S_MIC_CHANNEL I2S_CHANNEL_FMT_ONLY_LEFT
-i2s_config_t i2s_config = {
-  .mode = (i2s_mode_t)(I2S_MODE_MASTER | I2S_MODE_RX),
-  .sample_rate = SAMPLE_RATE,
-  .bits_per_sample = I2S_BITS_PER_SAMPLE_32BIT,
-  .channel_format = I2S_CHANNEL_FMT_ONLY_LEFT,
-  .communication_format = I2S_COMM_FORMAT_I2S,
-  .intr_alloc_flags = ESP_INTR_FLAG_LEVEL1,
-  .dma_buf_count = 4,
-  .dma_buf_len = 1024,
-  .use_apll = false,
-  .tx_desc_auto_clear = false,
-  .fixed_mclk = 0
-};
-i2s_pin_config_t i2s_mic_pins = {
-  .bck_io_num = pin_i2s_sck,
-  .ws_io_num = pin_i2s_ws,
-  .data_out_num = I2S_PIN_NO_CHANGE,
-  .data_in_num = pin_i2s_sd
-};
+// #define SAMPLE_BUFFER_SIZE 512
+// #define SAMPLE_RATE 8000
+// #define I2S_MIC_CHANNEL I2S_CHANNEL_FMT_ONLY_LEFT
+// i2s_config_t i2s_config = {
+//   .mode = (i2s_mode_t)(I2S_MODE_MASTER | I2S_MODE_RX),
+//   .sample_rate = SAMPLE_RATE,
+//   .bits_per_sample = I2S_BITS_PER_SAMPLE_32BIT,
+//   .channel_format = I2S_CHANNEL_FMT_ONLY_LEFT,
+//   .communication_format = I2S_COMM_FORMAT_I2S,
+//   .intr_alloc_flags = ESP_INTR_FLAG_LEVEL1,
+//   .dma_buf_count = 4,
+//   .dma_buf_len = 1024,
+//   .use_apll = false,
+//   .tx_desc_auto_clear = false,
+//   .fixed_mclk = 0
+// };
+// i2s_pin_config_t i2s_mic_pins = {
+//   .bck_io_num = pin_i2s_sck,
+//   .ws_io_num = pin_i2s_ws,
+//   .data_out_num = I2S_PIN_NO_CHANGE,
+//   .data_in_num = pin_i2s_sd
+// };
 
 void setup() {
   Serial.begin(115200);
@@ -251,26 +251,28 @@ void setup() {
   }
 
   // ---------- Microphone ----------
-  Serial.println();
-  Serial.println("Microphone Test");
-  Serial.println("*** Check if changes output are detected after noise (numbers should be larger both in positive and negative)");
-  Serial.println("*** If you want to check if there are noise-waves, open the Serial Plotter");
-  Serial.println("*** If you whistle there should be a visible sine-wave in the Serial Plotter");
-  Serial.println("*** To Start, press ENTER");
-  Serial.println("*** Press ENTER again to quit this test");
-  while (Serial.read() != '\n')
-    ;
-  i2s_driver_install(I2S_NUM_0, &i2s_config, 0, NULL);
-  i2s_set_pin(I2S_NUM_0, &i2s_mic_pins);
-  int32_t raw_samples[SAMPLE_BUFFER_SIZE];
-  while (Serial.read() != '\n') {
-    size_t bytes_read = 0;
-    i2s_read(I2S_NUM_0, raw_samples, sizeof(int32_t) * SAMPLE_BUFFER_SIZE, &bytes_read, portMAX_DELAY);
-    int samples_read = bytes_read / sizeof(int32_t);
-    for (int i = 0; i < samples_read; i++) {
-      Serial.printf("%ld\n", raw_samples[i]);
-    }
-  }
+  // TODO: fix microphone code with new I2S driver that replaced the legacy I2S driver
+  Serial.println("Microphone Test => Currently there is a software bug that prevents the microphone from being tested.");
+  // Serial.println();
+  // Serial.println("Microphone Test");
+  // Serial.println("*** Check if changes output are detected after noise (numbers should be larger both in positive and negative)");
+  // Serial.println("*** If you want to check if there are noise-waves, open the Serial Plotter");
+  // Serial.println("*** If you whistle there should be a visible sine-wave in the Serial Plotter");
+  // Serial.println("*** To Start, press ENTER");
+  // Serial.println("*** Press ENTER again to quit this test");
+  // while (Serial.read() != '\n')
+  //   ;
+  // i2s_driver_install(I2S_NUM_0, &i2s_config, 0, NULL);
+  // i2s_set_pin(I2S_NUM_0, &i2s_mic_pins);
+  // int32_t raw_samples[SAMPLE_BUFFER_SIZE];
+  // while (Serial.read() != '\n') {
+  //   size_t bytes_read = 0;
+  //   i2s_read(I2S_NUM_0, raw_samples, sizeof(int32_t) * SAMPLE_BUFFER_SIZE, &bytes_read, portMAX_DELAY);
+  //   int samples_read = bytes_read / sizeof(int32_t);
+  //   for (int i = 0; i < samples_read; i++) {
+  //     Serial.printf("%ld\n", raw_samples[i]);
+  //   }
+  // }
 
 
   Serial.println();
